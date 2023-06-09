3050f3d7847bdc74c1f66eed6088e17a21fcaeb9
sadrakhaksari <128301351+sadrakhaksari@users.noreply.github.com>
2023-06-10 03:16:37 +0330

    last debug

--- backend.py
+++ backend.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
+import requests
+from flask import session
 
 
-app = Flask(ــnameــ)
+
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
 """
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
@@ -66,32 +72,44 @@ def AddItemPage():
 def index():
     return render_template('index.html', clothing_list=clothing_list, electronics_list=electronics_list, kitchenware_list=kitchenware_list)
 
+
 @app.route('/ShoppingCartPage', methods=['GET', 'POST'])
+
+def get_exchange_rates(): 
+    url = 'https://openexchangerates.org/api/latest.json' 
+    params = {'app_id': 'YOUR_APP_ID'} 
+    response = requests.get(url, params=params) 
+    return response.json()['rates'] 
+
 def ShoppingCartPage():
+    cart_items = session.get('ShoppingCartPage', [])
+    cart_products = []
+
+    total_price_in_rials = 0
+    for item in cart_items:
+        product = next((p for p in products if p['product_id'] == item['product_id']), None)
+        if product:
+            product['quantity'] = item['quantity']
+            cart_products.append(product)
+            total_price_in_rials += int(product['price_IRR']) * product['quantity']
+
+    exchange_rates = get_exchange_rates()
+    usd_rate = exchange_rates['USD']
+    total_price_in_usd = round(total_price_in_rials / usd_rate, 2)
+
     if request.method == 'POST':
-        product_ids = request.form.getlist('product_id')
-        quantities = request.form.getlist('quantity')
+        product_id = request.form.get('product_id')
+        quantity = int(request.form.get('quantity'))
 
-        for product_id, quantity in zip(product_ids, quantities):
-            for product in products:
-                if product['product_id'] == product_id:
-                    product['quantity'] = quantity
-                    break
-    total_price = 0
-    for product in products:
-        price = float(product['price_IRR'])
-        quantity = int(product['quantity'])
-        total_price += price * quantity
+        if product_id and quantity:
+            cart_items.append({'product_id': product_id, 'quantity': quantity})
+            session['ShoppingCartPage'] = cart_items
+            return redirect(url_for('ShoppingCartPage'))
+        else:
+            return "Invalid form data", 400
+    else:
+        return render_template('ShoppingCartPage.html', products=cart_products, total_price_in_rials=total_price_in_rials, total_price_in_usd=total_price_in_usd)
 
-    return render_template('ShoppingCartPage.html', total_price=total_price)
 
-    
-    
-    
-if ــnameــ == 'ــmainــ':
+if __name__ == '__main__':
     app.run(debug=True)
-    
-    
-    
-    
- 
